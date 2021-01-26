393c34967bea5bc6c91655dbbf3be3232b80dbfa
Joao C Barbosa <joaao@outlook.com>
2021-01-26 12:27:09 -0300

    feat: start the server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+require('dotenv/config');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`App running on port ${process.env.APP_PORT}`);
+});
